2c03507f3dfe430c01cbbe8514f4a312537a5400
Yusra Salam <yusra.salam126@gmail.com>
2021-11-30 13:38:03 -0500

    modifying and editing classes

--- ClientOSChatApp/src/sample/Controller.java
+++ ClientOSChatApp/src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class Controller {
+
+public class Controller implements Initializable{
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
 }
--- ClientOSChatApp/src/sample/Main.java
+++ ClientOSChatApp/src/sample/Main.java
@@ -11,11 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello 3313 World");
+        primaryStage.setTitle("Chat App");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
