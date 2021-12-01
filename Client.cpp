9a07cb3edf82c00a4619d2d84f51ff22e86084cc
AndrewLou2 <louandrew123@gmail.com>
2021-12-01 18:28:50 -0500

    added new working files

--- Client.cpp
+++ Client.cpp
@@ -7,49 +7,58 @@
 
 using namespace Sync;
 
+// This thread handles the connection to the server
+class ClientThread : public Thread
+{
+private:
+	// Reference to our connected socket
+	Socket& socket;
+
+	// Data to send to server
+	ByteArray data;
+	std::string data_str;
+public:
+	ClientThread(Socket& socket)
+	: socket(socket)
+	{}
+
+	~ClientThread()
+	{}
+
+	virtual long ThreadMain()
+	{
+		int result = socket.Open();
+		std::cout << "Please input your data (done to exit): ";
+		std::cout.flush();
+
+		// Get the data
+		std::getline(std::cin, data_str);
+		data = ByteArray(data_str);
+
+		// Write to the server
+		socket.Write(data);
+
+		// Get the response
+		socket.Read(data);
+		data_str = data.ToString();
+		std::cout << "Server Response: " << data_str << std::endl;
+		return 0;
+	}
+};
+
 int main(void)
 {
-	// Welcome the user
-	std::cout << "SE3313 Lab 3 Client" << std::endl;
+	// Welcome the user 
+	std::cout << "SE3313 Final Client" << std::endl;
 
 	// Create our socket
-	Socket socket("127.0.0.1", 3000);
-	std::string input;
-	ByteArray data;
-	try
+	Socket socket("127.0.0.1", 1236);
+	ClientThread clientThread(socket);
+	while(1)
 	{
-		socket.Open(); //open the socket
-		while (true)
-		{
-			std::cout << "Enter a string, or done to terminate the client, or close to shutdown the server." << std::endl;
-			getline(std::cin, input);
-
-			data = *new ByteArray(input);
-			if (input == "done" || input == "close")
-			{
-				socket.Write(data);
-				std::cout << "Client closed." << std::endl;
-				break;
-			}
-			else
-			{
-				socket.Write(data);
-				socket.Read(data);
-				if (data.ToString() == "close" || data.ToString() == "") //if Close recieved or an empty string, server is closed
-				{
-					std::cout << "Server Closed... Terminating Client" << std::endl;
-					break; //shutdown if sent Close
-				}
-
-				std::cout << data.ToString() << std::endl;
-			}
-		}
-		socket.Close();
+		sleep(1);
 	}
-	catch (...)
-	{ //Server not open, send error msg
-		std::string errorMsg = "Server is not open... terminating Client";
-		std::cout << errorMsg << std::endl;
-	}
+	socket.Close();
+
 	return 0;
-};
+}
