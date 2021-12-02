a363cd29e09a277544bea93030b06af16c63f335
alicesung2001 <alicesung2001@gmail.com>
2021-12-01 23:07:14 -0500

    modifications

--- chatosapp.py
+++ chatosapp.py
@@ -14,11 +14,11 @@ def receive():
         except OSError:
             break
 def send(event=None):  # Binders pass the event.
-    msg = my_msg.get()
-    my_msg.set("")      # Clears text field.
+    msg = client_msg.get()
+    client_msg.set("")      # Clears text field.
     global current_room
     if msg == "{quit}":
-        client_socket.send(bytes(my_username.get() + " has closed OS Messenger App!", "utf8"))
+        client_socket.send(bytes(my_username.get() + " has left the chat!", "utf8"))
         client_socket.close()
         top.quit()
         return
@@ -26,23 +26,23 @@ def send(event=None):  # Binders pass the event.
 
 
 # Send quit message to the server.
-def on_closing(event=None):
+def close_window(event=None):
     # Send server quit message.
-    my_msg.set("{quit}")
+    client_msg.set("{quit}")
     send()
 
-
-def change_room():
+# handles the client switching chatrooms
+def switch_room():
     global current_room
     current_room = ((chatRoomSelected.get()).split(' '))[2]
     client_socket.send(bytes("/" + current_room, "utf8"))
     msg_list.delete(0, tkinter.END)
-    msg_list.insert(tkinter.END, "You are now in room " + str(current_room))
+    msg_list.insert(tkinter.END, "you've switched to room " + str(current_room))
     msg_list.see(tkinter.END)
 
 
 # Global variables.
-number_of_rooms = 0
+num_rooms = 0
 current_room = 0
 
 #create a GUI app using tkinter
@@ -53,16 +53,16 @@ top.geometry("660x600")
 
 
 #add a title to the app
-top.title("Chat Messenger App")
+top.title("chat room app")
 
 #adding a frame to the top
 messages_frame = tkinter.Frame(top)
 
 #messages to be sent
-my_msg = tkinter.StringVar()  
+client_msg = tkinter.StringVar()  
 
 #setting default "" for messages
-my_msg.set("")
+client_msg.set("")
 
 #username to be set
 my_username = tkinter.StringVar()
@@ -71,12 +71,12 @@ my_username = tkinter.StringVar()
 my_username.set("")
 
 # creating a label for the title of the app
-title_label = tkinter.Label(top, text='Welcome to Chat APP!', font=('calibre',20,'bold'),justify=CENTER)
+title_label = tkinter.Label(top, text='welcome to chat room!', font=('calibre',20,'bold'),justify=CENTER)
 title_label.config(bg='#BACC81',fg='#FFFCF7')
 title_label.grid(row=0,column=0,pady=15)
 
 # creating a label for username using widget Label
-username_label = tkinter.Label(top, text = 'Username', font=('calibre',10, 'bold'))
+username_label = tkinter.Label(top, text = 'username', font=('calibre',10, 'bold'))
 username_label.config(bg="#D2BF55", fg= "#FFF")
 username_label.grid(row=1,column=0,pady=10,padx= 250,ipadx=30,sticky=E)
 
@@ -97,17 +97,17 @@ messages_frame.grid(row=2,column=0,padx=20)
 
 
 # creating an entry field for user to input their message
-entry_field = tkinter.Entry(top, textvariable=my_msg, width=87)
+entry_field = tkinter.Entry(top, textvariable=client_msg, width=87)
 entry_field.bind("<Return>", send)
 entry_field.config(bg='#FFFCF7')
 entry_field.grid(row=3,column=0,pady=20,padx=20,sticky=W)
 
 # creating a send button #uncomment somehting here
-send_button = tkinter.Button(top, text="Send", width=10, height=1, font=('calibre',10,'bold'), command=send)
+send_button = tkinter.Button(top, text="send", width=10, height=1, font=('calibre',10,'bold'), command=send)
 send_button.config(bg="#013A20", fg= "#FFFCF7",activeforeground= "#FFF", activebackground="#D2BF55")
 send_button.grid(row=3,column=0,pady=20,padx=20,sticky=E)
 
-top.protocol("WM_DELETE_WINDOW", on_closing)
+top.protocol("WM_DELETE_WINDOW", close_window)
 
 # Socket with given AWS parameters.
 HOST = "127.0.0.1"
@@ -120,17 +120,17 @@ client_socket.connect(ADDR)
 
 # Server response of number of rooms available and generate drop down list.
 first_msg = client_socket.recv(BUFFER_SIZE).decode("utf8")
-number_of_rooms = int(first_msg)
+num_rooms = int(first_msg)
 chatRoomSelected = tkinter.StringVar(top)
-chatRoomSelected.set("Chat Rooms")
+chatRoomSelected.set("chat rooms")
 rooms_list = []
-for i in range(number_of_rooms):
-    rooms_list.append("Chat Room " + str(i + 1))
+for i in range(num_rooms):
+    rooms_list.append("chat room " + str(i + 1))
 
 chat_rooms = tkinter.OptionMenu(top, chatRoomSelected, *rooms_list)
 chat_rooms.config(bg="#D2BF55", fg= "#FFF",activeforeground= "#FFFCF7", activebackground="#013A20",font=('calibre',10,'bold'))
 chat_rooms.grid(row=1,column=0,pady=20, padx=20,sticky=W)
-change_button = tkinter.Button(top, text="Change Room",font=('calibre',10,'bold'),bg="#D2BF55", fg= "#FFF",activeforeground= "#FFFCF7", activebackground="#013A20", command=change_room)
+change_button = tkinter.Button(top, text="switch room",font=('calibre',10,'bold'),bg="#D2BF55", fg= "#FFF",activeforeground= "#FFFCF7", activebackground="#013A20", command=switch_room)
 change_button.grid(row=1,column=0,pady=20, padx=150, sticky=W)
 
 
